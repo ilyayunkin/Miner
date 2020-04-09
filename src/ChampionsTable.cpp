234174a212182fb9e9fc75958d92f0856c825aed
ilyayunkin <ilya.yunkin@ya.ru>
2020-04-09 11:49:48 +0600

    ChampionsTable replaced

--- src/ChampionsTable.cpp
+++ src/ChampionsTable.cpp
@@ -7,16 +7,16 @@
 #include <QSettings>
 #include <QInputDialog>
 #include <QMessageBox>
-#include <QDateTime>
 
-ChampionsTable::ChampionsTable(QString company,
-                               QString application,
-                               int count,
-                               bool greater)
-    : key("champions"),
-      count(count),
-      greater(greater),
-      company(company),
+#include <algorithm>
+
+namespace
+{
+const QString key("champions");
+}
+
+ChampionsTable::ChampionsTable(QString company, QString application)
+    : company(company),
       application(application)
 {
     QCoreApplication::setOrganizationName(company);
@@ -30,69 +30,44 @@ ChampionsTable::ChampionsTable(QString company,
     QString championsString(champions.toString());
     if(champions != defaultValue){
         QStringList list = championsString.split(";");
-        foreach (QString item, list) {
+        for (QString item: list) {
             QStringList itemList = item.split("#");
             if(itemList.size() == 2){
-                long long way = itemList[0].toLongLong();
+                long long coins = itemList[0].toLongLong();
                 QString name = itemList[1];
-                map.insert(way, name);
+                map.push_back(Entry{coins, name});
             }
         }
     }
 }
 
-void ChampionsTable::setResult(long long coins)
+void ChampionsTable::setCoins(long long coins)
 {
-    QList<int> sortedCoins = map.keys();
-    if(greater){
-        qSort(sortedCoins.begin(), sortedCoins.end(), qGreater<int>());
-    }else{
-        qSort(sortedCoins.begin(), sortedCoins.end(), qLess<int>());
-    }
+    auto greaterCoinsFunctor= [&](const Entry &e1, const Entry &e2)
+    {
+        return e1.coins > e2.coins;
+    };
 
-    if((sortedCoins.size() < count) || isChampion(sortedCoins, coins)){
+    std::sort(map.begin(), map.end(), greaterCoinsFunctor);
+    if((map.size() < COUNT) || (coins > map.rbegin()->coins)){
         QString name = QInputDialog::getText(0, "Enter your name", "Name");
-
-        map.insert(coins, name);
-        sortedCoins = map.keys();
-
-        if(greater){
-            qSort(sortedCoins.begin(), sortedCoins.end(), qGreater<int>());
-        }else{
-            qSort(sortedCoins.begin(), sortedCoins.end(), qLess<int>());
-        }
-
+        map.push_back({coins, name});
+        std::sort(map.begin(), map.end(), greaterCoinsFunctor);
         QString outputTable;
         QString text;
-        int i = 1;
-        foreach(int result, sortedCoins){
-            if(i != 1){
+        for(unsigned i = 0; i < COUNT && i < map.size(); ++i){
+            const Entry &e = map[i];
+            if(i != 0){
                 outputTable += ";";
             }
-            QString name(map[result]);
-            outputTable += QString("%1#%2").arg(result).arg(name);
-            text += QString("%1 %2\n").arg(result).arg(name);
-            if(i == count){
-                break;
-            }
-            i++;
+            outputTable += QString("%1#%2").arg(e.coins).arg(e.name);
+            text += QString("%1. %2 %3\n").arg(i + 1).arg(e.name).arg(e.coins);
+
         }
-        text += QString("%1 (c) %2 %3").
-                arg(application).
-                arg(company).
-                arg(QDateTime::currentDateTime().date().year());
+        text += QString("%1 (c) %2 2016").arg(application).arg(company);
         QMessageBox::information(0, "Champions", text);
 
         QSettings s;
         s.setValue(key, outputTable);
-    }
-}
-
-bool ChampionsTable::isChampion(const QList<int> &sortedCoins, long long coins)
-{
-    if(greater){
-        return coins > sortedCoins.last();
-    }else{
-        return coins < sortedCoins.last();
     }
 }
--- src/ChampionsTable.h
+++ src/ChampionsTable.h
@@ -5,26 +5,25 @@
 #define CHAMPIONSTABLE_H
 
 #include <QString>
-#include <QMap>
+#include <vector>
 
 class ChampionsTable
 {
-    QString key;
-    QMap<int, QString> map;
+    struct Entry
+    {
+        long long coins;
+        QString name;
+    };
 
-    const int count;
-    const bool greater;
-
+    std::vector<Entry> map;
+    enum{
+        COUNT = 10
+    };
     QString company;
     QString application;
-
-    bool isChampion(const QList<int> &sortedCoins, long long coins);
 public:
-    ChampionsTable(QString company,
-                   QString application,
-                   int count = 15,
-                   bool greater = true);
-    void setResult(long long coins);
+    ChampionsTable(QString company, QString application);
+    void setCoins(long long coins);
 };
 
 #endif // CHAMPIONSTABLE_H
